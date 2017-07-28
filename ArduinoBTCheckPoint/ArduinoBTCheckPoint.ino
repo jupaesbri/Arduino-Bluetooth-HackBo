032c8354e9cf8b7083d561aa76c8a4cb16c9032b
Juan Pablo España Briceño <juanpablo.esbri@gmail.com>
2017-07-28 14:35:59 -0400

    Integracion protocolo HTTP

--- ArduinoBTCheckPoint/ArduinoBTCheckPoint.ino
+++ ArduinoBTCheckPoint/ArduinoBTCheckPoint.ino
@@ -1,15 +1,30 @@
+#include <Arduino.h>
+
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+
+const char* ssid     = "HUAWEIP8";
+const char* password = "focobol555";
+
 String direccion = "";
 char a;
 
 
+ESP8266WiFiMulti WiFiMulti;
+
 void setup() 
-{
+{ 
   //INICIALIZAMOS LA COMUNICACION SERIAL
   Serial.begin(38400); //COMUNICACION SERIAL DEL MODULO
   //Serial.begin(38400); //PUERTO DE INTERFAZ
   delay(2000);
 
-  
+  //WiFi.begin(ssid,password);
+  WiFiMulti.addAP("HUAWEIP8", "focobol555");
+  delay(500);
+
   //Serial.println("Comenzando...");
   //RESETEAMOS AT
   Serial.println("AT+RESET");
@@ -27,6 +42,7 @@ void setup()
   //VACIAMOS EL BUFFER
   //while(Serial.available())
   //Serial1.read();
+
 }
 
 void loop() 
@@ -70,11 +86,34 @@ void loop()
 //AQUI SE ENVIARAN LOS DATOS AL SERVIDOR
 void Imprimir()
 {
-  if(direccion[1] != 'K')
+  if((WiFiMulti.run() == WL_CONNECTED))
   {
-    Serial.println(direccion);
-  }
-  delay(50);
-  direccion == "";
+    HTTPClient http;
+    
+    if(direccion[1] != 'K')
+    {
+      //Serial.println(direccion);
+      if(direccion[5] == '9')
+      {
+        http.begin("http://checkpointhackbo.azurewebsites.net/api/RegistroPuntosApi/1"); //HTTP
+        delay(100);
+        int httpCode = http.GET();
+        //Serial.println("ENVIO1");
+      }
+
+      if(direccion[5] == '2')
+      {
+        http.begin("http://checkpointhackbo.azurewebsites.net/api/RegistroPuntosApi/2"); //HTTP
+        int httpCode = http.GET();
+        //Serial.println("ENVIO2");
+      }
+      //direccion = "1@" + direccion;
+      //Serial.println(direccion);
+      //client.println(direccion);
+    }
+      http.end();
+      delay(10000);
+      direccion == ""; 
+    }
 }
 
